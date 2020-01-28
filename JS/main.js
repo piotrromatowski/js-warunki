391d10aec94c301fba87990cc4dfce9c3874d676
Piotr <piotr.romatowski@gmail.com>
2020-01-28 17:30:53 +0100

    js-warunki

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,4 @@ let cyfra = 12;
 if(cyfra > 10){
     console.log('Podana liczba jest większa od 10');
 } else
-    {String('Podana liczba nie jest większa od 10')};
+    {console.log('Podana liczba nie jest większa od 10')};
